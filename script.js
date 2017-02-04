51c25fd29e4c2242f83d8ce59247e990743f43ad
unknown <grey5@ptc-jva007-006.ptc.local>
2017-02-04 12:48:25 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function Bird(type, price, quantity) {
+    this.type = type,
+        this.price = price,
+        this.quantity = quantity,
+        this.changePrice = function(price){this.price = price;},
+        this.changeQuantity = function(quantity){this.quantity = quantity;},
+        this.sell = function(q){this.quantity -= q;}
+}
+
